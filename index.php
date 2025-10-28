16f9de9eaa522586ad43eb2dbed3ed58d86786ae
Kirishan-Thavagnanaranjan <Thavagnanaranjankirishan@gmail.com>
2025-10-28 16:54:31 +0530

    Add initial layout for school system site
    Created index.php with basic HTML structure and navigation for students, subjects, and grades. Added index.css for layout and responsive styling of header, navigation, content, and footer.

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>School System</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="assets\css\index.css">
+
+</head>
+<body>
+<header>
+  <h2>School System</h2>
+</header>
+
+<section>
+  <nav>
+    <ul>
+      <li><a href="#">Students</a></li>
+      <li><a href="#">Subjects</a></li>
+      <li><a href="#">Grades</a></li>
+    </ul>
+  </nav>
+  
+  <article>
+    
+  </article>
+</section>
+
+<footer>
+  <p>Footer</p>
+</footer>
+
+</body>
+</html>
+
